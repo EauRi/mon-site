6ea696aeea5724f21d0aebfad728e5211a2fed58
EauRi <aurianegal@gmx.com>
2025-03-09 14:08:05 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,6 @@ const supabase = createClient(
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('Service Worker enregistré'))
-      .catch(err => console.log('Erreur Service Worker', err));
-  });
-}
-
 // Liste des mois
 const months = [
   "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
